5fc132f6e10ed009dc368fbc29359ea21519eefa
Diego Miguel Pereira <corgandp@gmail.com>
2020-08-06 01:02:30 -0300

    Revised TeacherForm styles.css

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,5 @@
 import React, { InputHTMLAttributes } from 'react';
-import './styless.css';
+import './styles.css';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,34 +1,43 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import './styles.css';
+import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader
         title="Que incrível que você quer dar aulas."
-        description="O primeiro passo é preencher esse formulário de inscreição"
+        description="O primeiro passo é preencher esse formulário de inscrição"
       />
       <main>
+
         <fieldset>
           <legend>Seus dados</legend>
-
-          <div className="imput-block">
-            <label htmlFor="name">Nome completo</label>
-            <input type="text" id="name" />
-          </div>
-
-          <div className="imput-block">
-            <label htmlFor="avatar">Avatar</label>
-            <input type="text" id="avatar" />
-          </div>
-
-          <div className="imput-block">
-            <label htmlFor="whatsapp">Whatsapp</label>
-            <input type="text" id="whatsapp" />
-          </div>
-
+          <Input name="name" label="Nome completo"></Input>
+          <Input name="name" label="Avatar"></Input>
+          <Input name="name" type="number" label="Whatsapp"></Input>
         </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input name="subject" label="Matéria"></Input>
+          <Input name="cost" label="Seu custo por aula"></Input>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso Importante" />
+          Importante! <br />
+          Preencha todos os dados
+          </p>
+
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+
       </main>
     </div>
   )
