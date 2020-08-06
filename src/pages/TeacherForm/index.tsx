69cf60a0e30001ddd0f6ec6fff941b1b0af34d6b
Diego Miguel Pereira <corgandp@gmail.com>
2020-08-05 22:36:57 -0300

    Created TeacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -4,7 +4,31 @@ import PageHeader from '../../components/PageHeader';
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas."/>
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscreição"
+      />
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <div className="imput-block">
+            <label htmlFor="name">Nome completo</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className="imput-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+
+          <div className="imput-block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+
+        </fieldset>
+      </main>
     </div>
   )
 }
